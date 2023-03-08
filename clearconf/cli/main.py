3c7a1c8bb1a8fd2e1e0966cc5edd05abd8c9bf23
andrearosasco <andrearosasco.ar@gmail.com>
2023-03-08 13:31:51 +0100

    changed package name, added .clearconf, started implementing list

--- clearconf/cli/main.py
+++ clearconf/cli/main.py
@@ -2,13 +2,15 @@ import shutil
 from pathlib import Path
 import os
 
-import confort
-
+import clearconf
 import typer
+import json
+
+from clearconf.cli.utils.file import find_config, load_config
 
 app = typer.Typer()
 
-stub_path = Path(os.path.abspath(confort.__file__)).parent / "stubs/config.py"
+stub_path = Path(os.path.abspath(clearconf.__file__)).parent / "stubs/config.py"
 
 
 @app.command()
@@ -19,10 +21,28 @@ def init():
 
     cfg_root.mkdir()
     (cfg_root / '__init__.py').touch()
-    print(stub_path)
     shutil.copy(stub_path, cfg_root)
 
-    print(f"Create confort .config file")
+    cconf_configs = Path('.clearconf')
+    if cconf_configs.exists():
+        raise FileExistsError(f'File {cconf_configs.as_posix()} already exists.')
+
+    cconf_configs.touch()
+
+    content = {'cfg_root': cfg_root.as_posix()}
+    with cconf_configs.open('w+') as f:
+        json.dump(content, f, indent=4)
+        f.write('\n')
+
+@app.command()
+def list():
+    cconf_conf_path = find_config()
+    cconf_conf = load_config(cconf_conf_path)
+
+    # List all files ending with _cfg in the cfg_root directory
+    for file in Path(cconf_conf['cfg_root']).glob('*'):
+        print(file)
+
 
 
 @app.callback()
